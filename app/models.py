330786f9010adbd3572596afaf63e8068a4f8bdd
Gonzalo Concepción Megía <gonzaloconcepcionmegia@ua.pt>
2018-04-10 17:33:14 +0100

    something

--- app/models.py
+++ app/models.py
@@ -23,9 +23,9 @@ def get_absolute_url(self):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products')
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE,)
     name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField
+    slug = models.SlugField()
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -37,4 +37,10 @@ class Product(models.Model):
 class Meta:
     ordering = ('-created',)
     index_together = (('id', 'slug'),)
+
+def __str__(self):
+    return self.name
+
+def get_absolute_url(self):
+    return reverse('shop:product_detail', args=[self.id, self.slug])
 
