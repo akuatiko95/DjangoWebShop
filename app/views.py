3f2abb252be6d4a5e7610833a455894024115638
stevanmatovic <stevan.matovic95@gmail.com>
2018-04-10 23:38:38 +0100

    Display of items and cart

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from datetime import datetime
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 # our views
 def product_list(request, category_slug=None):
