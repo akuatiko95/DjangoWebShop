28f05b4d5f4416d216dddcafe7ad6e1adfcfecc4
Gonzalo Concepción Megía <gonzaloconcepcionmegia@ua.pt>
2018-04-10 19:55:14 +0100

    Errors fixed

--- webproj/urls.py
+++ webproj/urls.py
@@ -22,14 +22,13 @@ from django.conf.urls.static import static
 from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('login/', views.login, name='login'),
     path('layout/', views.layout, name='layout'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('shop.urls', namespace='shop'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(('app.urls','app'), namespace='app')),
 ]
 
 if settings.DEBUG:
