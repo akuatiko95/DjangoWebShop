6e41bb7d13f8bf7caf61fc138ace563ca775f014
Gonzalo Concepción Megía <gonzaloconcepcionmegia@ua.pt>
2018-04-10 17:59:00 +0100

    Database examples

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from datetime import datetime
+from .models import Category, Product
 
+# our views
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category)
 
 # Create your views here.
 def home(request):
