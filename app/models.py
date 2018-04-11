f30f38992ae14185e58022eff918b6899c22665c
stevanmatovic <stevan.matovic95@gmail.com>
2018-04-11 15:51:46 +0100

    fixed error with classes, products added

--- app/models.py
+++ app/models.py
@@ -9,16 +9,16 @@ class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
-class Meta:
-    ordering = ('name',)
-    verbose_name = 'category'
-    verbose_name_plural = 'categories'
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-def get_absolute_url(self):
-    return reverse('shop:product_list_by_category', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('app:product_list_by_category', args=[self.slug])
 
 
 
@@ -34,13 +34,13 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-class Meta:
-    ordering = ('-created',)
-    index_together = (('id', 'slug'),)
+    class Meta:
+        ordering = ('-created',)
+        index_together = (('id', 'slug'),)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-def get_absolute_url(self):
-    return reverse('shop:product_detail', args=[self.id, self.slug])
+    def get_absolute_url(self):
+        return reverse('app:product_detail', args=[self.id, self.slug])
 
